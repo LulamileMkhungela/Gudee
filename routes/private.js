e74348b6c73b0d8dd0cd141c00b62dd0235f7eb3
Thabiso-Timzo <87146271+Thabiso-Timzo@users.noreply.github.com>
2022-02-09 20:20:01 +0200

    Delete routes directory

--- routes/private.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getPrivateRoute } = require("../controllers/private");
-const { protect } = require("../middleware/auth");
-
-router.route("/private").get(protect, getPrivateRoute);
-
-module.exports = router;
