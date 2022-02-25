56acef1f0cf7d3b3b6d36c50b1c4168d3d81915c
Thabiso Hlatshwayo <mr.tt.hlatshwayo@gmail.com>
2022-02-25 15:47:51 +0200

    Created 404 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,13 @@
-import React, {useContext, useState} from 'react';
-import {BrowserRouter, Route, Switch, useHistory} from 'react-router-dom';
-import {useForm} from 'react-hook-form';
+import React from 'react';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './global.css';
 
-import {RegisterContext} from './Helper/Context';
-import Login from './components/screens/Login';
-import Registration from './components/screens/Registration';
 import ForgotPassword from './components/screens/ForgotPassword';
 import ResetPassword from './components/screens/ResetPassword';
-import NavExplore from './components/screens/NavExplore';
 import AddProfile from './components/screens/AddProfile';
 import AddProfileTwo from './components/screens/AddProfileTwo';
 import Profile from './components/screens/Profile';
-import {Redirect} from 'react-router-dom';
 import About from './components/screens/Aboutpage';
 import Product from './components/screens/ProductCategory';
 import Forgot from './components/screens/Forgot';
@@ -25,50 +19,48 @@ import LoginScreen from './components/screens/Login';
 import RegisterScreen from './components/screens/Registration';
 import AddedtoCard from './components/screens/AddedtoCard';
 import {ProductList} from './components/screens/ProductList';
-import {Chatbox} from './components/screens/Chatbox';
 import {PrivateScreen} from './components/screens/PrivateScreen';
 import {PrivateRoute} from './components/privates/PrivateRoute';
-import {AuthContext} from "./components/context/AuthContext";
 import Messenger from "./components/pages/Messenger";
 import ActivationEmail from './components/screens/ActivationEmail'
-import NotFound from "./components/screens/notFound";
 import Electronics from './components/screens/Electonics';
 import Freebies from './components/screens/Freebies';
 import Gaming from './components/screens/Gaming';
 import Stationery from './components/screens/Stationery';
 import Outdoors from './components/screens/Outdoors';
-
+import PageNotFound from './components/screens/PageNotFound';
 
 const App = () => {
-    const {user} = useContext(AuthContext);
 
     return (            
-        <BrowserRouter>       
-            <PrivateRoute exact path="/" component={PrivateScreen}/>
-            <Route axact path="/login" component={LoginScreen}/>
-            <Route axact path="/loginseller" component={LoginSeller}/>
-            <Route axact path="/registerseller" component={Registrationseller}/>
-            <Route axact path="/forgot" component={Forgot}/>
-            <Route axact path="/reset:resetoken" component={Reset}/>
-            <Route axact path="/register" component={RegisterScreen}/>
-            <Route path="/cart" component={AddedtoCard}/>
-            <Route path="/addprofiletwo" component={AddProfileTwo}/>
-            <Route path="/addprofile" component={AddProfile}/>
-            <Route path="/resetpassword/:resetToken" component={ResetPassword}/>
-            <Route path="/forgotpassword" component={ForgotPassword}/>
-            <Route path="/product" component={Product}/>
-            <Route path="/profile" component={Profile}/>
-            <Route path="/store" component={ProductList}/>
-            <Route path="/additemstosell" component={ItmesToSell}/>
-            <Route path="/about" component={About}/>
-            <Route path="/store-electronics" component={Electronics}/>
-            <Route path="/store-freebies" component={Freebies}/>
-            <Route path="/store-gaming" component={Gaming}/>
-            <Route path="/store-stationery" component={Stationery}/>
-            <Route path="/store-outdoors" component={Outdoors}/>
-            <Route path="/messages" component={Messenger}/>
-            <Route path="/user/activate/:activation_token" component={ActivationEmail} exact/>
-            <Route component={NotFound}> </Route>
+        <BrowserRouter>  
+            <Switch>     
+                <PrivateRoute exact path="/" component={PrivateScreen}/>
+                <Route axact path="/login" component={LoginScreen}/>
+                <Route axact path="/loginseller" component={LoginSeller}/>
+                <Route axact path="/registerseller" component={Registrationseller}/>
+                <Route axact path="/forgot" component={Forgot}/>
+                <Route axact path="/reset:resetoken" component={Reset}/>
+                <Route axact path="/register" component={RegisterScreen}/>
+                <Route path="/cart" component={AddedtoCard}/>
+                <Route path="/addprofiletwo" component={AddProfileTwo}/>
+                <Route path="/addprofile" component={AddProfile}/>
+                <Route path="/resetpassword/:resetToken" component={ResetPassword}/>
+                <Route path="/forgotpassword" component={ForgotPassword}/>
+                <Route path="/product" component={Product}/>
+                <Route path="/profile" component={Profile}/>
+                <Route path="/store" component={ProductList}/>
+                <Route path="/additemstosell" component={ItmesToSell}/>
+                <Route path="/about" component={About}/>
+                <Route path="/store-electronics" component={Electronics}/>
+                <Route path="/store-freebies" component={Freebies}/>
+                <Route path="/store-gaming" component={Gaming}/>
+                <Route path="/store-stationery" component={Stationery}/>
+                <Route path="/store-outdoors" component={Outdoors}/>
+                <Route path="/messages" component={Messenger}/>
+                <Route component={PageNotFound}/>
+                <Route path="/user/activate/:activation_token" component={ActivationEmail} exact/>
+            </Switch>
         </BrowserRouter>
     );
 }
---
+++ frontend/src/components/screens/PageNotFound.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+import './PageNotFound.scss';
+import Image from '../../images/15.png'
+
+function PageNotFound() {
+  return (
+    <>
+        <div className="page-not-found">
+            <img className="Image-file" src={Image} alt="page not found" />
+            <h1>Ooops!! The page you were looking for doesn't exist.</h1>
+            <div className="button">
+                <Link to="/store"><button className="btn-home">Go to the store page</button></Link>
+                <Link to="/login"><button className="btn-home">Go to login page</button></Link>
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default PageNotFound;
